86fe8e16e229b2dad98d3cc92691d2b04711e50b
RobbeDeWolf <robbe.dewolf@student.vives.be>
2023-06-06 11:22:14 +0200

    Controllers en servicelaag Angular side

---
+++ ASP.NETCoreWebApplication1/ClientApp/src/app/services/shared.service.ts
@@ -0,0 +1,17 @@
+﻿import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService{
+
+  constructor( private http: HttpClient) {
+  }
+
+  //Getters
+  GetAllGenes() {return this.http.get("api/Gene/GetAllGenes")};
+  GetGeneByGuid(id: any) {return this.http.get("api/Gene/GetGeneByGuid/" + id)};
+  //Setters
+}
---
+++ ASP.NETCoreWebApplication1/Controllers/GeneController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCoreWebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GeneController : Controller
+{
+    [HttpGet("[action]")]
+    public ActionResult GetAllGenes()
+    {
+        return Ok();
+    }
+
+    [HttpGet("[action]/{id}")]
+    public ActionResult GetGeneByGuid(Guid id)
+    {
+        return Ok();
+    }
+}
